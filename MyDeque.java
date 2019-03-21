a0bbce68827bce6bd35e717cb7b09f11abf866f1
grace-mao23 <gmao00@stuy.edu>
2019-03-21 09:40:58 -0400

    Empty methods and suppresswarnings added

---
+++ MyDeque.java
@@ -0,0 +1,46 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public void addFirst(E element){
+
+  }
+
+  public void addLast(E element){
+
+  }
+
+  public E removeFirst(E element){
+    return element;
+  }
+
+  public E removeLast(E element){
+    return element;
+  }
+
+  public E getFirst(E element){
+    return element;
+  }
+  
+  public E getLast(E element){
+    return element;
+  }
+}
