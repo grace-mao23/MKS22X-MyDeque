ff67be4d0f0c0436df2522715e1e14e5f4889254
grace-mao23 <gmao00@stuy.edu>
2019-03-25 10:13:34 -0400

    Other operations completed and tested

--- Calculator.java
+++ Calculator.java
@@ -16,13 +16,21 @@ public class Calculator{
           double first = stack.removeLast();
           stack.addLast(first + second);
         } else if (t.equals("-")) {
-
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first - second);
         } else if (t.equals("*")) {
-
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first * second);
         } else if (t.equals("/")) {
-
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first / second);
         } else if (t.equals("%")) {
-
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first % second);
         } else {
           double d = Double.parseDouble(t);
           stack.addLast(d);
@@ -32,7 +40,10 @@ public class Calculator{
     }
 
     public static void main(String[] args) {
-      System.out.println(Calculator.eval("3 4 +"));
+      System.out.println(Calculator.eval("3 4 +")); // 7.0
+      System.out.println(Calculator.eval("9 3 4 + -")); //2.0
+      System.out.println(Calculator.eval("11 3 4 + - 2 % 8 + 2 / 3 *")); // 12.0
+      System.out.println(Calculator.eval("1 1 + 2 3 4 + - *")); // -10.0
     }
 
 }
