96dd230782b1e9b1bf810ab073bc2a3c7246e1f4
grace-mao23 <gmao00@stuy.edu>
2019-03-25 10:03:01 -0400

    Split string into tokens

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,15 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
+      String[] tokens = s.split(" ");
+      for (String reg : tokens) {
+        System.out.println(reg);
+      }
       return -1.0;
+    }
+
+    public static void main(String[] args) {
+      Calculator.eval("3 4 +");
     }
 
 }
